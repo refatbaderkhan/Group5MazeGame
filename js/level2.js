f2d5c70367a2eb89817041c8a25d32ee696d27c2
zeinaCodes <eye_likemoon@hotmail.com>
2023-07-15 22:09:49 +0300

    finished maze 2 structure

--- js/level2.js
+++ js/level2.js
@@ -85,11 +85,48 @@ function createMaze() {
     build_vertically(20, 80, 18)
 
     // bottom
-    build_horizontally(60, 610, 40)
-    
+    build_horizontally(20, 610, 41)
+
     // right
     build_vertically(1270, 50, 18)
 
+    // barriers
+    build_vertically(140, 50, 6)
+    build_horizontally(80, 260, 4)
+    build_vertically(80, 140, 4)
+    build_vertically(200, 80, 17)
+    build_horizontally(50, 350, 4 )
+    build_horizontally(50, 530, 4)
+    build_horizontally(80, 440, 4)
+    build_horizontally(200, 290, 6) 
+    build_horizontally(260, 350, 4)
+    build_horizontally(320, 410, 4) 
+    build_vertically(320, 440, 1)
+    build_horizontally(320, 470, 5) 
+    build_horizontally(260, 530, 10)
+    build_vertically(260, 380, 5)
+    build_horizontally(260, 110, 6)
+    build_vertically(260, 110, 5)
+    build_horizontally(260, 230, 6)
+    build_horizontally(320 ,170, 5)
+    build_vertically(470, 50, 15)
+    build_vertically(410, 230, 6)
+    build_vertically(530, 80, 16)
+    build_horizontally(560, 80, 4)
+    build_vertically(650, 80, 16)
+    build_vertically(590, 140, 16)
+    build_vertically(830, 80, 18)
+    build_horizontally(830, 80, 15)
+    build_horizontally(920, 170, 14)
+    build_horizontally(830, 260, 14)
+    build_horizontally(920, 350, 14)
+    build_horizontally(830,440 , 14)
+    build_horizontally(920, 530, 14)
+    build_vertically(740, 50, 17)
+
+    // build_horizontally(290, 80, 5)
+    // build_horizontally(290, 140, 5)
+    // build_vertically(290, 110, 1)
     wallsData.forEach(function (wall) {
         walls.create(wall.x, wall.y, 'wall');
     });
