8ed66b14659ee949aa1c637dafc0aff702ada67a
Hasan <hasan@Hasans-MacBook-Air.local>
2023-07-16 15:40:19 +0300

    added event listner for vertical scrolling which increases speed by 2.5x for the text compared to background

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
 const text = document.getElementById('text')
 const desert = document.getElementById('desert')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    
+    text.style.marginTop = value * 2.5 + 'px';
+})
