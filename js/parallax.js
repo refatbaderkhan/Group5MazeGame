88f80572c27ae7c547f45cfa1db483300d596892
Hasan <hasan@Hasans-MacBook-Air.local>
2023-07-16 17:00:17 +0300

    added an offset when scroll is finished to launch level1.html

--- js/parallax.js
+++ js/parallax.js
@@ -3,10 +3,14 @@ let text = document.getElementById('text');
 let gateleft = document.getElementById('gate-left');
 let gateright = document.getElementById('gate-right');
 
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     let value = window.scrollY;
     logo.style.marginTop = value * 1.5 + 'px';
     gateleft.style.left = value * -0.5 + 'px';
     gateright.style.left = value * 0.5 + 'px';
 
-})
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        window.location.href = 'level1.html';
+    }
+
+});
