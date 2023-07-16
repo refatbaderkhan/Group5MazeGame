7bdae0640c02de0e16a65e5735186e4f5c0953c5
Hasan <hasan@Hasans-MacBook-Air.local>
2023-07-16 15:41:37 +0300

    removed background image selector from dom

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 const text = document.getElementById('text')
-const desert = document.getElementById('desert')
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    
+
     text.style.marginTop = value * 2.5 + 'px';
 })
