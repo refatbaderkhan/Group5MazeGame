96ca99defa7d3fde431b7656291df13c86c0beea
refatbaderkhan <refatbaderkhan@gmail.com>
2023-07-16 18:10:30 +0300

    refat level 2 modifications

--- js/level2.js
+++ js/level2.js
@@ -1,7 +1,9 @@
+//for further comments and explainations, checkout level 1 script
 const config = {
     type: Phaser.AUTO,
     width: '100%',
     height: '100%',
+    backgroundColor : '#F8F0E3',
     physics: {
         default: 'arcade',
         arcade: {
@@ -15,11 +17,17 @@ const config = {
         update: update
     }
 };
+
 const game = new Phaser.Game(config);
+
+
 function preload() {
     this.load.image('player', '../assets/images/player_level2.png');
     this.load.image('wall', '../assets/images/wall_level2.png');
-    this.load.image('goal', '../assets/images/player_level2.png');
+    this.load.image('goal', '../assets/images/goal_level2.png');
+    this.load.image('trap','/assets/images/hole_level2.png');
+    this.load.audio('scream', '/assets/audio/scream.mp3');
+    this.load.audio('music_level2', '/assets/audio/music_level2.mp3');
 }
 
 // function to slide modal to screen
@@ -32,20 +40,59 @@ function unhideModal() {
     });
 }
 
+
 let player;
 let walls;
 let goal;
+let traps;
+let timer;
+let timerText;
+let gameText;
+
 
 function create() {
+    const music = this.sound.add('music_level2', {loop: false });
+    music.play();
+
     player = this.physics.add.sprite(20, 50, 'player');
+
     player.setCollideWorldBounds(true);
 
     walls = this.physics.add.staticGroup();
+
+    traps = this.physics.add.staticGroup();
+
+    timer = this.time.addEvent({
+        delay: 120000,
+        callback: endGameTime,
+        callbackScope: this
+      });
+
+    timerText = this.add.text(this.cameras.main.width / 2, 10, 'Time: 30', {
+      fontFamily: 'Arial',
+      fontSize: 24,
+      color: '#000000',
+      backgroundColor: '#F5F5DC',
+      fontWeight: 'bold'
+    }).setOrigin(0.5, 0);
+
+    timerText.setDepth(1);
+
     createMaze();
+
+    gameText = this.add.text(this.cameras.main.width / 2, 625, 'Guide your kitty to get some food ** Hurry up and be on time! otherwise your cat will starve :( ** Watch out for the cactus! kitty hates them ** Press ESC anytime to quit the game', {
+      fontFamily: 'Arial',
+      fontSize: 16,
+      color: '#000000',
+      backgroundColor: '#F5F5DC',
+      fontWeight: 'bold'
+    }).setOrigin(0.5, 0);
 
     goal = this.physics.add.sprite(1270, 600, 'goal');
 
     this.physics.add.collider(player, walls);
+
+    this.physics.add.overlap(player, traps, endGameTrap, null, this);
 
     this.physics.add.overlap(player, goal, function () {
         unhideModal();
@@ -53,6 +100,8 @@ function create() {
     }, null, this);
 
     cursors = this.input.keyboard.createCursorKeys();
+
+    quit = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
 }
 
 function update() {
@@ -70,10 +119,36 @@ function update() {
     } else {
         player.setVelocityX(0);
     }
+
+    if (Phaser.Input.Keyboard.JustDown(quit)) {
+    endGameQuit();
+    }
+
+    const remainingTime = Math.round((timer.delay - timer.getElapsed()) / 1000);
+    timerText.setText('Time: ' + remainingTime);
+}
+
+//timer end game function
+function endGameTime() {
+  alert('Time is up! Game Over.');
+  game.destroy();
+}
+
+//trap end game function with sound effect
+function endGameTrap() {
+  const scream = this.sound.add('scream', {loop:false});
+  scream.play();
+  alert('laaaaaaaaa2');
+  resetGame();
+}
+
+//esc quit game function
+function endGameQuit() {
+  alert('See you later!.');
+  game.destroy();
 }
 
 function createMaze() {
-
     // var increment = 30;
     var wallsData = [];
     function build_vertically(x, y, blocks_num) {
@@ -134,10 +209,81 @@ function createMaze() {
     build_horizontally(920, 530, 14)
     build_vertically(740, 50, 17)
 
-    // build_horizontally(290, 80, 5)
-    // build_horizontally(290, 140, 5)
-    // build_vertically(290, 110, 1)
     wallsData.forEach(function (wall) {
-        walls.create(wall.x, wall.y, 'wall');
+        walls.create(wall.x, wall.y, 'wall').setDepth(0);
     });
+
+
+    var trapsData = [
+        { x: 830, y: 260 },
+        { x: 920, y: 350 },
+        { x: 830, y: 440 },
+        { x: 50, y: 350 },
+        { x: 50, y: 530 },
+        { x: 80, y: 440 },
+        { x: 80, y: 140 },
+        { x: 200, y: 80 },
+        { x: 410, y: 80 },
+        { x: 320, y: 170 },
+        { x: 410, y: 230 },
+        { x: 350, y: 290 },
+        { x: 350, y: 350 },
+        { x: 290, y: 530 },
+        { x: 140, y: 530 },
+        { x: 470, y: 470 },
+        { x: 410, y: 470 },
+        { x: 530, y: 410 },
+        { x: 470, y: 350 },
+        { x: 530, y: 290 },
+        { x: 470, y: 200 },
+        { x: 530, y: 80 },
+        { x: 650, y: 80 },
+        { x: 590, y: 140 },
+        { x: 590, y: 200 },
+        { x: 590, y: 320 },
+        { x: 590, y: 440 },
+        { x: 590, y: 560 },
+        { x: 590, y: 320 },
+        { x: 590, y: 320 },
+        { x: 650, y: 170 },
+        { x: 650, y: 230 },
+        { x: 650, y: 350 },
+        { x: 650, y: 410 },
+        { x: 650, y: 500 },
+        { x: 740, y: 560 },
+        { x: 740, y: 470 },
+        { x: 740, y: 410 },
+        { x: 740, y: 290 },
+        { x: 740, y: 200 },
+        { x: 740, y: 110 },
+        { x: 830, y: 580 },
+        { x: 830, y: 80 },
+        { x: 1190, y: 80 },
+        { x: 1010, y: 80 },
+        { x: 920, y: 140 },
+        { x: 1190, y: 260 },
+        { x: 950, y: 260 },
+        { x: 1190, y: 260 },
+        { x: 1190, y: 350 },
+        { x: 980, y: 350 },
+        { x: 1010, y: 410 },
+        { x: 1130, y: 440 },
+        { x: 1190, y: 440 },
+        { x: 1160, y: 500 },
+        { x: 1010, y: 530 },
+        { x: 920, y: 530 },
+        { x: 890, y: 530 },
+        { x: 1040, y: 590 },
+        { x: 1130, y: 560 },
+    ];
+    
+    // loop that iterates over each object in the trapsData array. For each object, the traps.create method is called to create a trap sprite at the specified x and y coordinates using the 'trap' image.
+    trapsData.forEach(function(trap) {
+    traps.create(trap.x, trap.y, 'trap').setDepth(1);
+    });
+
+
+    
+
+    
 }
