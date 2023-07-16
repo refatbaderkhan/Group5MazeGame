87cce3055f83572031bb8b4ee46096b382033a40
zeinaCodes <eye_likemoon@hotmail.com>
2023-07-16 11:40:54 +0300

    fixed maze2 structure

--- js/level2.js
+++ js/level2.js
@@ -38,7 +38,7 @@ function create() {
     this.physics.add.collider(player, walls);
 
     this.physics.add.overlap(player, goal, function () {
-        alert('Congratulations! Kitty is Happy Now.');
+        // unhideModal();
         game.destroy();
     }, null, this);
 
@@ -72,7 +72,7 @@ function createMaze() {
         }
     }
 
-    function build_horizontally( x, y, blocks_num) {
+    function build_horizontally(x, y, blocks_num) {
         for (var i = 0; i < blocks_num; i++) {
             wallsData.push({ x: x + 30 * i, y: y });
         }
@@ -95,32 +95,32 @@ function createMaze() {
     build_horizontally(80, 260, 4)
     build_vertically(80, 140, 4)
     build_vertically(200, 80, 17)
-    build_horizontally(50, 350, 4 )
+    build_horizontally(50, 350, 4)
     build_horizontally(50, 530, 4)
     build_horizontally(80, 440, 4)
-    build_horizontally(200, 290, 6) 
+    build_horizontally(200, 290, 6)
     build_horizontally(260, 350, 4)
-    build_horizontally(320, 410, 4) 
+    build_horizontally(320, 410, 4)
     build_vertically(320, 440, 1)
-    build_horizontally(320, 470, 5) 
+    build_horizontally(320, 470, 5)
     build_horizontally(260, 530, 10)
     build_vertically(260, 380, 5)
     build_horizontally(260, 110, 6)
     build_vertically(260, 110, 5)
     build_horizontally(260, 230, 6)
-    build_horizontally(320 ,170, 5)
+    build_horizontally(320, 170, 5)
     build_vertically(470, 50, 15)
     build_vertically(410, 230, 6)
     build_vertically(530, 80, 16)
     build_horizontally(560, 80, 4)
-    build_vertically(650, 80, 16)
+    build_vertically(650, 80, 18)
     build_vertically(590, 140, 16)
     build_vertically(830, 80, 18)
     build_horizontally(830, 80, 13)
     build_horizontally(920, 170, 14)
-    build_horizontally(830, 260, 14)
+    build_horizontally(830, 260, 13)
     build_horizontally(920, 350, 14)
-    build_horizontally(830,440 , 14)
+    build_horizontally(830, 440, 13)
     build_horizontally(920, 530, 14)
     build_vertically(740, 50, 17)
 
